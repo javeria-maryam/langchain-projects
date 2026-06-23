000ba34295b428e8c22e012773ac0dfeef61ec3b
javeria-maryam <mjaveria777@gmail.com>
2026-06-23 12:47:57 +0500

    add conversation memory to chatbot

--- first_chain.py
+++ first_chain.py
@@ -1,5 +1,6 @@
 from langchain_groq import ChatGroq
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage
 from dotenv import load_dotenv
 import os
 
@@ -12,14 +13,25 @@ llm = ChatGroq(
 
 prompt = ChatPromptTemplate.from_messages([
     ("system", "You are a helpful assistant."),
+    MessagesPlaceholder(variable_name="history"),
     ("human", "{question}")
 ])
 
 chain = prompt | llm
 
+history = []
+
 while True:
     question = input("You: ")
     if question.lower() == "quit":
         break
-    response = chain.invoke({"question": question})
+    
+    response = chain.invoke({
+        "question": question,
+        "history": history
+    })
+    
     print(f"Bot: {response.content}\n")
+    
+    history.append(HumanMessage(content=question))
+    history.append(AIMessage(content=response.content))
